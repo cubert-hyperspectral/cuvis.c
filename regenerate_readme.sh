f37ef6fe02513e2c554561db11b9bf02e2993125
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-04-18 11:51:47 +0200

    added automatic init file regeneration

---
+++ regenerate_readme.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Navigate to the repository root
+cd $(git rev-parse --show-toplevel)
+
+fetch_file_from_branch() {
+	local file_name=$1
+	local default_branch=$2
+	
+	local branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+	# Try to fetch the file from the current branch
+	local status_code=$(curl -o tmp/${file_path} -s -w "%{http_code}" "https://raw.githubusercontent.com/cubert-hyperspectral/cuvis.sdk/${branch_name}/readme/${file_path}")
+	
+	# If the file was not found (HTTP 404), fetch it from the default branch
+    if [ "$status_code" -eq 404 ]; then
+        curl -o tmp/${file_path} "https://raw.githubusercontent.com/cubert-hyperspectral/cuvis.sdk/${default_branch}/readme/${file_path}"
+    fi
+}
+
+fetch_file_from_branch "logo.md" "main"
+fetch_file_from_branch "header.md" "main"
+fetch_file_from_branch "footer.md" "main"
+
+cat tmp/logo.md > README.md
+printf "# cuvis.c\n" >> README.md
+cat tmp/header.md >> README.md
+printf "\n" >> README.md
+cat README.in >> README.md
+cat tmp/footer.md >> README.md
